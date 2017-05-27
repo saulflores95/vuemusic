0132368d778350c713366a6b320c7151a7c73bfa
Saul Flores <saulpolo95@gmail.com>
2017-05-26 18:35:13 -0700

    Fixed logo url

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='https://saulflores95.github.io/vuemusic/dist/logo.png')
     h1 Vue Music
     select(v-model="selectedCountry")
       option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
